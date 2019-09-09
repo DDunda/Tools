49668661239b438efd3a1e22c4f7f4bc3dfce0bd
DDunda <deaconfarrow.email@gmail.com>
2019-09-09 23:07:47 +1000

    Added an array and binary classes Adhered to some conventions Added some documentation Changed the project from exe to a library

--- Program.cs
+++ Program.cs
@@ -5,25 +5,280 @@ using System.Collections.Generic;
 using System.Linq;
 namespace Tools
 {
-	public class Program
+	public static class EasyBinary
 	{
-		public static void Main()
+		/// <summary>
+		/// Converts a byte to 8 bools.
+		/// </summary>
+		/// <param name="dat">The bool to parse.</param>
+		/// <param name="MSBo">Most significant bit order: Determines if 1 is at the most significant bit (leftmost).</param>
+		/// <returns></returns>
+		public static bool[] SplitByte(byte binary, bool MSBo = false)
 		{
-			Vector2 pointer = Vector2.Unit;
-			for (int i = -100; i < 100; i++) Console.WriteLine(FastMath.RadianAngleClamp(i / 180.0 * Math.PI));
-			for (int i = 0; i < 720; i++)
-				Console.WriteLine($"{i} {FastMath.Cos(i * (float)Math.PI / 180)} {FastMath.Sin(i * (float)Math.PI / 180)} {Vector2.VectorFromAngleAndSize(i).ToString()} {Math.Cos(i * Math.PI / 180)} {Math.Sin(i * Math.PI / 180)}");
-			Dictionary<string, string> r = EasyRegex.CSVParseDictionary("test1, name1\n      test2     ,   name2     \n ,");
-			Dictionary<string, string> d = new Dictionary<string, string> { { "test1", "name1" }, { "test2", "name2" } };
-			bool equal = r.Keys.Count == d.Keys.Count && r.Values.Count == d.Values.Count;
-			if (equal)
-				foreach (string key in r.Keys)
-					if (!d.Keys.Contains(key) | d[key] != r[key])
+			// MSB
+			if (MSBo)
+				return new bool[] {
+					(binary & 1  ) == 1,
+					(binary & 2  ) == 2,
+					(binary & 4  ) == 4,
+					(binary & 8  ) == 8,
+					(binary & 16 ) == 16,
+					(binary & 32 ) == 32,
+					(binary & 64 ) == 64,
+					(binary & 128) == 128
+				};
+
+			// LSB
+			return new bool[] {
+				(binary & 128) == 128,
+				(binary & 64 ) == 64,
+				(binary & 32 ) == 32,
+				(binary & 16 ) == 16,
+				(binary & 8  ) == 8,
+				(binary & 4  ) == 4,
+				(binary & 2  ) == 2,
+				(binary & 1  ) == 1
+			};
+		}
+
+		public static byte[] MakeBytes(bool[] bits, bool strict = false, bool MSBo = false)
+		{
+			int dataLen = bits.Length >> 3;
+			byte[] data;
+
+			if ((bits.Length & 7) != 0)
+			{
+				if (strict)
+					throw new System.ArgumentException($"In strict mode the length of the passed bit array must be a multiple of 8 to construct a byte array. {bits.Length} bits were passed.");
+				else
+					data = new byte[dataLen + 1];
+			}
+			else
+			{
+				data = new byte[dataLen];
+			}
+
+			if (MSBo)
+			{
+				for (int i = 0; i < dataLen; i++)
+					data[i] = MakeByte(EasyArray.SubArray(bits, bits.Length - 9 - i << 3, 8), strict, true);
+				if ((bits.Length & 7) != 0)
+					data[dataLen] = MakeByte(EasyArray.SubArray(bits, 0, bits.Length & 7), strict, true);
+			}
+			else
+			{
+				for (int i = 0; i < dataLen; i++)
+					data[i] = MakeByte(EasyArray.SubArray(bits, i << 3, 8), strict, false);
+				if ((bits.Length & 7) != 0)
+					data[dataLen] = MakeByte(EasyArray.SubArray(bits, dataLen << 3, bits.Length & 7), strict, false);
+			}
+
+			return data;
+		}
+
+		public static byte MakeByte(bool[] bits, bool strict = false, bool MSBo = false)
+		{
+			if (bits.Length != 8)
+			{
+				if (strict)
+					throw new System.ArgumentException($"In strict mode a full 8 bits must be passed to construct a byte. {bits.Length} bits were passed.");
+
+				if (bits.Length > 8)
+					bits = bits.SubArray(bits.Length - 8, 8);
+
+				else
+					Array.Resize(ref bits, 8);
+			}
+			if (MSBo)
+			{
+				return (byte)(
+					(bits[7] ? 128 : 0) |
+					(bits[6] ? 64 : 0) |
+					(bits[5] ? 32 : 0) |
+					(bits[4] ? 16 : 0) |
+					(bits[3] ? 8 : 0) |
+					(bits[2] ? 4 : 0) |
+					(bits[1] ? 2 : 0) |
+					(bits[0] ? 1 : 0)
+				);
+			}
+			return (byte)(
+				(bits[0] ? 128 : 0) |
+				(bits[1] ? 64 : 0) |
+				(bits[2] ? 32 : 0) |
+				(bits[3] ? 16 : 0) |
+				(bits[4] ? 8 : 0) |
+				(bits[5] ? 4 : 0) |
+				(bits[6] ? 2 : 0) |
+				(bits[7] ? 1 : 0)
+			);
+		}
+
+		public static string ToString(byte binary)
+		{
+			string strOut = "";
+
+			foreach (bool bit in SplitByte(binary))
+				strOut += bit ? "1" : "0";
+
+			return strOut;
+		}
+
+		public static string ToString(byte[] binary)
+		{
+			string strOut = "";
+
+			foreach (byte data in binary)
+				foreach (bool bit in SplitByte(data))
+					strOut += bit ? "1" : "0";
+
+			return strOut;
+		}
+
+		public static string ToString(bool[] bits)
+		{
+			string strOut = "";
+
+			foreach (bool bit in bits)
+				strOut += bit ? "1" : "0";
+
+			return strOut;
+		}
+
+
+		/// <summary>Shifts a byte left, except bits are wrapped and not truncated</summary>
+		public static byte CyclicLeftShift(byte data, int shift)
+		{
+			shift &= 0x7;
+			byte wrap = (byte)(data >> (8 - shift));
+			return (byte)((data << shift) | wrap);
+		}
+		/// <summary>Shifts an sbyte left, except bits are wrapped and not truncated</summary>
+		public static sbyte CyclicLeftShift(sbyte data, int shift)
+		{
+			shift &= 0x7;
+			byte num = (byte)data;
+			byte wrap = (byte)(num >> (8 - shift));
+			return (sbyte)((num << shift) | wrap);
+		}
+		/// <summary>Shifts an uint left, except bits are wrapped and not truncated</summary>
+		public static uint CyclicLeftShift(uint data, int shift)
+		{
+			shift &= 0x1F;
+			uint wrap = data >> (32 - shift);
+			return (data << shift) | wrap;
+		}
+		/// <summary>Shifts an int left, except bits are wrapped and not truncated</summary>
+		public static int CyclicLeftShift(int data, int shift)
+		{
+			shift &= 0x1F;
+			uint num = (uint)data;
+			uint wrap = num >> (32 - shift);
+			return (int)((num << shift) | wrap);
+		}
+		/// <summary>Shifts an ulong left, except bits are wrapped and not truncated</summary>
+		public static ulong CyclicLeftShift(ulong data, int shift)
+		{
+			shift &= 0x3F;
+			ulong wrap = data >> (64 - shift);
+			return (data << shift) | wrap;
+		}
+		/// <summary>Shifts a long left, except bits are wrapped and not truncated</summary>
+		public static long CyclicLeftShift(long data, int shift)
+		{
+			shift &= 0x3F;
+			ulong num = (ulong)data;
+			ulong wrap = num >> (64 - shift);
+			return (long)((num << shift) | wrap);
+		}
+
+		/// <summary>Shifts a byte right, except bits are wrapped and not truncated</summary>
+		public static byte CyclicRightShift(byte data, int shift)
+		{
+			shift &= 0x7;
+			byte wrap = (byte)(data << (8 - shift));
+			return (byte)((data >> shift) | wrap);
+		}
+		/// <summary>Shifts an sbyte right, except bits are wrapped and not truncated</summary>
+		public static sbyte CyclicRightShift(sbyte data, int shift)
+		{
+			shift &= 0x7;
+			byte num = (byte)data;
+			byte wrap = (byte)(num << (8 - shift));
+			return (sbyte)((num >> shift) | wrap);
+		}
+		/// <summary>Shifts an uint right, except bits are wrapped and not truncated</summary>
+		public static uint CyclicRightShift(uint data, int shift)
+		{
+			shift &= 0x1F;
+			uint wrap = data << (32 - shift);
+			return (data >> shift) | wrap;
+		}
+		/// <summary>Shifts an int right, except bits are wrapped and not truncated</summary>
+		public static int CyclicRightShift(int data, int shift)
+		{
+			shift &= 0x1F;
+			uint num = (uint)data;
+			uint wrap = num << (32 - shift);
+			return (int)((num >> shift) | wrap);
+		}
+		/// <summary>Shifts an ulong right, except bits are wrapped and not truncated</summary>
+		public static ulong CyclicRightShift(ulong data, int shift)
+		{
+			shift &= 0x3F;
+			ulong wrap = data << (64 - shift);
+			return (data >> shift) | wrap;
+		}
+		/// <summary>Shifts a long right, except bits are wrapped and not truncated</summary>
+		public static long CyclicRightShift(long data, int shift)
+		{
+			shift &= 0x3F;
+			ulong num = (ulong)data;
+			ulong wrap = num << (64 - shift);
+			return (long)((num >> shift) | wrap);
+		}
+	}
+
+	public static class EasyArray
+	{
+		///	<summary>
+		/// This method takes a specified subsection of an array.
+		/// </summary>
+		/// <example>
+		/// Here is an example use of the SubArray method:
+		/// <code>
+		/// int[] array = { 0, 1, 2, 3, 4, 5 };
+		/// int[] subArray = array.SubArray( 2, 3 ); // { 2, 3, 4 }
+		/// </code>
+		/// </example>
+		public static T[] SubArray<T>(this T[] data, int index, int length)
+		{
+			T[] result = new T[length];
+			Array.Copy(data, index, result, 0, length);
+			return result;
+		}
+
+		/// <summary>
+		/// Searches an array for all occurrances of selected targets.
+		/// </summary>
+		/// <example>
+		/// Here is an example use of the SubArray method:
+		/// <code>
+		/// string[] array = { "lemon", "apple", "orange", "peach"
+		/// int[] subArray = array.SubArray( 2, 3 ); // { 2, 3, 4 }
+		/// </code>
+		/// </example>
+		public static Dictionary<int, T> SearchFor<T>(this T[] data, params T[] targets)
+		{
+			Dictionary<int, T> hits = new Dictionary<int, T>();
+			for (int i = 0; i < data.Length; i++)
+				foreach (T target in targets)
+					if (data[i].Equals(target))
 					{
-						equal = false;
-						break;
+						hits[i] = data[i];
+						continue;
 					}
-			Console.WriteLine(equal);
+			return hits;
 		}
 	}
 
@@ -250,6 +505,10 @@ namespace Tools
 			Vector2 oV = (Vector2)o;
 			return oV.x == this.x && oV.y == this.y;
 		}
+		public override int GetHashCode()
+		{
+			return EasyBinary.CyclicLeftShift(x.GetHashCode(), 6) ^ y.GetHashCode();
+		}
 
 		public static Vector2 operator +(Vector2 v1, Vector2 v2)
 		{
@@ -317,6 +576,7 @@ namespace Tools
 		private double[] data;
 		public readonly int dimensions;
 
+		// To prevent somebody changing the size of the array, but not the corresponding dimension
 		public double this[int index]
 		{
 			get => data[index];
@@ -381,10 +641,13 @@ namespace Tools
 		}
 		public Matrix(VectorN vector) : this((double[])vector) { }
 
-		public static VectorN matrixToVec(Matrix m)
+		public static VectorN MatrixToVec(Matrix m)
 		{
 			double[] data = new double[m.height];
-			return new VectorN(m[0, 0], m[1, 0], m.height > 2 ? m[2, 0] : 0);
+
+			for (int i = 0; i < m.height; i++) data[i] = m[i, 0];
+
+			return new VectorN(data);
 		}
 
 		public void Print()
@@ -404,13 +667,13 @@ namespace Tools
 			}
 			Console.WriteLine();
 		}
-		public static VectorN matmulvec(Matrix a, VectorN vec)
+		public static VectorN MatmulVec(Matrix a, VectorN vec)
 		{
 			Matrix m = new Matrix(vec);
-			Matrix r = matmul(a, m);
-			return matrixToVec(r);
+			Matrix r = Matmul(a, m);
+			return MatrixToVec(r);
 		}
-		public static Matrix matmul(Matrix a, Matrix b)
+		public static Matrix Matmul(Matrix a, Matrix b)
 		{
 			int colsA = a.width;
 			int rowsA = a.height;
